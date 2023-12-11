3856acf42687415c598699688ab7ee6387d7901f
ljp-projects <131211966+ljp-projects@users.noreply.github.com>
2023-12-12 07:40:40 +1100

    stuff

---
+++ index.js
@@ -0,0 +1,50 @@
+let level = 1
+let xp = 0
+let xpUntilLevel = Math.ceil(100 * Math.log(10)) - 100
+
+const update = () => {
+	const xpElement = document.querySelector("#xp")
+  const levelElement = document.querySelector("#lvl")
+  const xpUntilLevelElement = document.querySelector("#xpulvl")
+  
+  xpElement.textContent = xp
+  levelElement.textContent = level
+  xpUntilLevelElement.textContent = xpUntilLevel
+}
+
+const addListeners = () => {
+	const xpUpButton = document.querySelector("#xpup")
+  
+  xpUpButton.onclick = function() {
+  	xp += level
+    
+    if (xp >= xpUntilLevel && document.getElementById("LVL-UP") == null) {
+    	const lvlUp = document.createElement("button")
+      
+      lvlUp.innerHTML = '<img src="./level+.svg" width="50" height="50">'
+      lvlUp.setAttribute("id", "LVL-UP")
+      
+      lvlUp.onclick = function() {
+      	xp -= xpUntilLevel
+        level++
+        
+        xpUntilLevel = Math.ceil(xpUntilLevel * Math.log(xpUntilLevel / 10))
+        
+        update()
+        
+        lvlUp.remove()
+      }
+      
+      document.body.appendChild(lvlUp)
+    }
+    
+    update()
+  }
+}
+
+const start = () => {
+	addListeners()
+  update()
+}
+
+start()
